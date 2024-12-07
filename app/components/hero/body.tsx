31bd565d8c38eee45bdcbf42cfca0be2386e44b6
AYANscyy2 <mayan6378@gmail.com.com>
2024-12-07 19:31:30 +0530

    feat: fixed copy button functionalities

--- app/components/hero/body.tsx
+++ app/components/hero/body.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 interface IMainProps {
   isUploaded: boolean;
-  uploadedfilesUrls?: {
+  uploadedfiles?: {
     secure_url: string;
     display_name: string;
     isCopied?: boolean;
@@ -17,38 +17,52 @@ interface IMainProps {
 
 const initialState: IMainProps = {
   isUploaded: false,
-  uploadedfilesUrls: [],
+  uploadedfiles: [],
   uploadCount: 0,
-  copyclickedcount: 0
+  copyclickedcount: 0,
 };
 
 export const Body = () => {
   const [state, setState] = useState(initialState);
 
+  const UpdateIsCopiedState = (url: string) => {
+    setState((prev) => ({
+      ...prev,
+      uploadedfiles: prev.uploadedfiles?.map((file) => {
+        if (file.secure_url === url) {
+          console.log("file.secure_url", file.secure_url);
+          return {
+            ...file,
+            isCopied: !file.isCopied,
+          };
+        }
+        return file;
+      }),
+    }));
+  };
+
   const handleCopy = (url: string) => {
-    navigator.clipboard.writeText(url).then(() => {
-      setState((prev) => ({
-        ...prev,
-        copyclickedcount: prev.copyclickedcount == 0 ? 1 : 0,
-        uploadedfilesUrls: prev.uploadedfilesUrls?.map((file) => {
-          if (file.secure_url === url) {
-            return {
-              ...file,
-              isCopied: true
-            };
-          }
-          return file;
-        })
-      }));
-    });
+    const notCopied = state.uploadedfiles?.filter((file) => !file.isCopied);
+    if (notCopied?.length) {
+      navigator.clipboard.writeText(url).then(() => {
+        UpdateIsCopiedState(url);
+      });
+    } else {
+      UpdateIsCopiedState(url);
+    }
   };
 
   return (
-    <div className="bg-[#2d3142] min-h-screen w-full flex flex-col gap-12 items-center py-10 px-4">
+    <div
+      style={{
+        backgroundImage: "linear-gradient(120deg ,#2d3142 ,black,black,black )",
+        backgroundSize: "cover",
+      }}
+      className="min-h-screen bg-current w-full flex flex-col gap-12 items-center py-10 px-4"
+    >
       <CldUploadWidget
         uploadPreset="uploadToCloudinary"
         onSuccess={(results) => {
-          console.log("results", results);
           const resultsInfo = results.info;
           if (
             resultsInfo &&
@@ -58,21 +72,23 @@ export const Body = () => {
             setState((prev) => ({
               ...prev,
               isUploaded: true,
-              uploadedfilesUrls: prev.uploadedfilesUrls
+              uploadedfiles: prev.uploadedfiles
                 ? [
-                    ...prev.uploadedfilesUrls,
+                    ...prev.uploadedfiles,
                     {
                       secure_url: resultsInfo.secure_url,
-                      display_name: resultsInfo.display_name ?? ""
-                    }
+                      display_name: resultsInfo.display_name ?? "",
+                      isCopied: false,
+                    },
                   ]
                 : [
                     {
                       secure_url: resultsInfo.secure_url,
-                      display_name: resultsInfo.display_name ?? ""
-                    }
+                      display_name: resultsInfo.display_name ?? "",
+                      isCopied: false,
+                    },
                   ],
-              uploadCount: prev.uploadCount ? prev.uploadCount + 1 : 1
+              uploadCount: prev.uploadCount ? prev.uploadCount + 1 : 1,
             }));
           } else {
             console.error(
@@ -94,31 +110,34 @@ export const Body = () => {
 
       {state.isUploaded && (
         <div className="w-full max-w-4xl bg-white/[0.1] rounded-lg p-5">
-          {state.uploadedfilesUrls?.map((file, index) => (
-            <div
-              key={index}
-              className={`flex flex-wrap sm:flex-nowrap items-center justify-between gap-5 p-4 border-b ${
-                index === 0 ? "rounded-t-lg" : ""
-              } text-white`}
-            >
-              <div className="text-lg sm:text-xl md:text-2xl font-semibold text-[#14213d] ">
-                {file.display_name}
-              </div>
-              <div className="text-sm sm:text-base text-white/70 break-words truncate">
-                {file.secure_url}
-              </div>
-              <div
-                className={`text-white  focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800  dark:focus:ring-gray-700 dark:border-gray-700 cursor-pointer ${
-                  file.isCopied && state.copyclickedcount === 1 ? "" : ""
-                }`}
-                onClick={() => handleCopy(file.secure_url)}
-              >
-                {file.isCopied && state.copyclickedcount === 1
-                  ? "Copied"
-                  : "Copy"}
-              </div>
-            </div>
-          ))}
+          {state.uploadedfiles?.map(
+            (file, index) => (
+              console.log(file),
+              (
+                <div
+                  key={index}
+                  className={`flex flex-wrap sm:flex-nowrap items-center justify-between gap-5 p-4 border-b ${
+                    index === 0 ? "rounded-t-lg" : ""
+                  } text-white`}
+                >
+                  <div className="text-lg sm:text-xl md:text-2xl max-md:truncate  font-semibold text-[#14213d] ">
+                    {file.display_name}
+                  </div>
+                  <div className="text-sm sm:text-base text-white/70 break-words truncate">
+                    {file.secure_url}
+                  </div>
+                  <div
+                    className={`text-white  focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800  dark:focus:ring-gray-700 dark:border-gray-700 cursor-pointer ${
+                      file.isCopied && state.copyclickedcount === 1 ? "" : ""
+                    }`}
+                    onClick={() => handleCopy(file.secure_url)}
+                  >
+                    {file.isCopied ? "Copied" : "Copy"}
+                  </div>
+                </div>
+              )
+            )
+          )}
         </div>
       )}
     </div>
